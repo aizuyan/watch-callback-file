413486a5ec247cb089e06633b1d28ae191bc18d8
yanruitao <luluyrt@163.com>
2016-07-04 17:18:53 +0800

    添加可选择接受的服务器

--- config.sh
+++ config.sh
@@ -1,8 +1,9 @@
-declare -A paths
+declare -A paths uploads
 paths[actions]=/usr/local/nginx/html/actions/
 #paths[conf]=/wwwroot/conf/app/napi/
 paths[controllers]=/usr/local/nginx/html/controllers/
 #paths[library]=/wwwroot/app/library/
 #paths[models]=/wwwroot/app/models/
 #paths[script]=/wwwroot/app/script/
-upload="http://10.211.55.3/receiver.php"
+upload[test1]="http://10.211.55.3/receiver.php"
+upload[test2]="http://10.211.55.3/receiver.php"
